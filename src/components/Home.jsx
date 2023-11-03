a768c7317ad0194c39a382d20b2287f0b1c0cc30
lozanoMiguel <lozanomiguel92@gmail.com>
2023-11-03 11:24:24 +0100

    reestructuracion del estilo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import StyledText from "./StyledText";
 import StyledView from "./StyledView";
-import { TouchableNativeFeedback, TextInput, Text, Button } from "react-native";
+import { TextInput, Button, View, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const Home = () => {
   const [nameFilter, setNameFilter] = useState("");
   const navigation = useNavigation();
+
   return (
-    <StyledView>
+    <View style={styles.container}>
       <StyledView height={"child"} width={"child"}>
         <StyledText color={"dark"}>
           In which coffee shop are you located?
@@ -26,8 +27,15 @@ const Home = () => {
           navigation.navigate("CoffeeShop", { coffeeName: nameFilter })
         }
       ></Button>
-    </StyledView>
+    </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default Home;
